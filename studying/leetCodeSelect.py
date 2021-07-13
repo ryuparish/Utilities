69000c2e974b952fe47636f20824d821d514a574
ryuparish <ryuparish1115@gmail.com>
2021-07-13 12:19:22 -0500

    Added puzzle picker and leetcode select

--- studying/leetCodeSelect.py
+++ studying/leetCodeSelect.py
@@ -1,7 +1,9 @@
 import random
 
 difficulties = ["Easy", "Medium", "Hard"]
+language = ["Java", "C++", "Python"]
 
-choice = random.choices(difficulties, weights=[50, 30, 20])[0]
+difficulty_choice = random.choices(difficulties, weights=[50, 30, 20])[0]
+language_choice = random.choices(language, weights=[70, 20, 10])[0]
 
-print(choice)
+print(difficulty_choice, "in", language_choice)
---
+++ studying/puzzlePicker.py
@@ -0,0 +1,6 @@
+from random import choices
+def main():
+    puzzles = ["I", "II", "III", "IV", "V", "VI", "VII", "IX", "X", "XI", "XII", "XIII", "XIV"]
+    print(choices(puzzles)[0])
+    return
+main()
