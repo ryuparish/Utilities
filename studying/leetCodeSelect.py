d624fafeffc72ddaf81f6fbe95b31a838e08298a
ryuparish <ryuparish1115@gmail.com>
2021-07-24 23:13:02 -0500

    Changed weights for leetCodeSelect because too many easys

--- studying/leetCodeSelect.py
+++ studying/leetCodeSelect.py
@@ -3,7 +3,8 @@ import random
 difficulties = ["Easy", "Medium", "Hard"]
 language = ["Java", "C++", "Python"]
 
-difficulty_choice = random.choices(difficulties, weights=[50, 30, 20])[0]
+#difficulty_choice = random.choices(difficulties, weights=[40, 35, 25])[0]
+difficulty_choice = random.choices(difficulties, weights=[30, 40, 30])[0]
 language_choice = random.choices(language, weights=[70, 20, 10])[0]
 
 print(difficulty_choice, "in", language_choice)
