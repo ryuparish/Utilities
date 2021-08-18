6bd9e4dfd99d29508e355ee13e1b14d1caa1778f
ryuparish <ryuparish1115@gmail.com>
2021-08-18 00:08:56 -0400

    Change leetCode to give more mediums

--- studying/leetCodeSelect.py
+++ studying/leetCodeSelect.py
@@ -4,7 +4,7 @@ difficulties = ["Easy", "Medium", "Hard"]
 language = ["Java", "C++", "Python"]
 
 #difficulty_choice = random.choices(difficulties, weights=[40, 35, 25])[0]
-difficulty_choice = random.choices(difficulties, weights=[30, 40, 30])[0]
+difficulty_choice = random.choices(difficulties, weights=[30, 60, 10])[0]
 language_choice = random.choices(language, weights=[70, 20, 10])[0]
 
 print(difficulty_choice, "in", language_choice)
