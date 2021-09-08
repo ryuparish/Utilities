b6d789ef8335db623920ae4567505c27f411671a
ryuparish <ryuparish1115@gmail.com>
2021-09-08 16:04:38 -0500

    Added cledge tools

--- studying/leetCodeSelect.py
+++ studying/leetCodeSelect.py
@@ -1,10 +1,11 @@
 import random
 
-difficulties = ["Easy", "Medium", "Hard"]
+difficulties = ["Medium", "Hard"]
+subjects = ["Dynamic Programming", "Trees", "Graph", "Sliding Window", "Array", "String", "Hash Table", "Stack", "Union Find", "Bit Manip", "Queues", "Heaps", "Recursion", "Backtracking", "DFS", "BFS"]
 language = ["Java", "C++", "Python"]
 
 #difficulty_choice = random.choices(difficulties, weights=[40, 35, 25])[0]
-difficulty_choice = random.choices(difficulties, weights=[30, 60, 10])[0]
+difficulty_choice = random.choices(difficulties, weights=[80, 20])[0]
 language_choice = random.choices(language, weights=[70, 20, 10])[0]
 
 print(difficulty_choice, "in", language_choice)
